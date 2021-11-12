4100571b6e647202d2b4031d987c18168b45c530
newbalance330 <92163228+newbalance330@users.noreply.github.com>
2021-11-12 02:38:26 +0000

    [Fix]index.htmlのレイアウト調整

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,12 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new
   end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books/:id'
+  end
 
   def show
   end
@@ -12,4 +18,10 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
