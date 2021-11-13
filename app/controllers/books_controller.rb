fd091e36a706f189444b0fc6e767e1f0ca774b10
newbalance330 <92163228+newbalance330@users.noreply.github.com>
2021-11-13 05:01:44 +0000

    [Add]cerate失敗時のエラー追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,14 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to  book_path(book.id)
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+     redirect_to  book_path(@book.id), notice: "Book was successfully created."
+    else
+     render :index
+
+    end
   end
 
   def show
@@ -20,18 +25,19 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book)
+    redirect_to book_path(book), notice: "Book was successfully updated."
   end
-  
-  
+
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path
+    redirect_to books_path, notice: "Book was successfully destroyed."
+
   end
 
   private
